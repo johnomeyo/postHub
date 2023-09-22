a0db62a74036e8662d3b76ddbd97197eb756c4f4
John <85222720+johnomeyo@users.noreply.github.com>
2023-09-22 13:13:40 +0300

    added the navigation bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
 import 'package:posthub/pages/home_page.dart';
+import 'package:posthub/pages/profile.dart';
+import 'package:posthub/pages/upload.dart';
 
 void main() {
   runApp(const MaterialApp(home: MyApp()));
@@ -11,8 +14,64 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData.light(),
-      home: const HomePage(),
+      theme: ThemeData.dark(),
+      home: const Home(),
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int selectedIndex = 0;
+  final List<Widget> pages = [
+    const HomePage(),
+    const Upload(),
+    const Profile()
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: pages[selectedIndex],
+      bottomNavigationBar: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Container(
+          decoration: BoxDecoration(
+              color: Colors.black, borderRadius: BorderRadius.circular(10)),
+          child: GNav(
+            gap: 8,
+            onTabChange: (value) {
+              setState(() {
+                selectedIndex = value;
+              });
+            },
+            backgroundColor: Colors.transparent,
+            color: Colors.white,
+            activeColor: Colors.white,
+            // textStyle: GoogleFonts.lato(),
+
+            tabs: const [
+              GButton(
+                icon: Icons.dashboard,
+                text: "Feed",
+              ),
+              GButton(
+                icon: Icons.add,
+                text: "Upload",
+              ),
+              GButton(
+                icon: Icons.person_2_outlined,
+                text: "Profile",
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
