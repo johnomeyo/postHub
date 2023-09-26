f1f3018dbb9570ed03d9770696a7767a8262a7fe
John <85222720+johnomeyo@users.noreply.github.com>
2023-09-26 13:25:48 +0300

    added auth

---
+++ lib/auth/auth.dart
@@ -0,0 +1,26 @@
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:posthub/auth/sign_in.dart';
+import 'package:posthub/pages/home_page.dart';
+
+class AuthPage extends StatelessWidget {
+  const AuthPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.deepPurple,
+      body: StreamBuilder<User?>(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return const HomePage();
+          } else {
+            return const LoginPage();
+          }
+        },
+      ),
+    );
+  }
+}
